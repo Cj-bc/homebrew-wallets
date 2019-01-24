33dece60cbb27fd0478b0fbed2e1b8baa8f3c14b
Cj-bc <cj-bc@y-modify.org>
2019-01-24 18:35:53 +0900

    Add: koto-qt> add sha256

--- Casks/koto-qt.rb
+++ Casks/koto-qt.rb
@@ -1,5 +1,6 @@
 cask 'koto-qt' do
   version '2.0.2'
+  sha256 '53b9ded8354aaa332e3002ee7cf06e5a1c6576dd92eead947ffdd8c81b6c537e'
 
   # github.com/KotoDevelopers/koto was verified as official when first introduced to the cask
   url "https://github.com/KotoDevelopers/koto/releases/download/v#{version}/koto-#{version}-osx-unsigned.dmg"
