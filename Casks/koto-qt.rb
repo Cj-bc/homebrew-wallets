37c2ad19ea3750e2871f3990ff1403ab8f2cd548
Cj-bc <cj-bc@y-modify.org>
2019-01-24 20:09:02 +0900

    Fix: koto-qt> rename to follow official spelling

--- Casks/koto-qt.rb
+++ Casks/koto-qt.rb
@@ -1,11 +1,11 @@
-cask 'koto-qt' do
+cask 'koto-Qt' do
   version '2.0.2'
   sha256 '53b9ded8354aaa332e3002ee7cf06e5a1c6576dd92eead947ffdd8c81b6c537e'
 
   # github.com/KotoDevelopers/koto was verified as official when first introduced to the cask
   url "https://github.com/KotoDevelopers/koto/releases/download/v#{version}/koto-#{version}-osx-unsigned.dmg"
   appcast 'https://github.com/KotoDevelopers/koto/releases.atom'
-  name 'koto-qt'
+  name 'koto-Qt'
   homepage 'https://ko-to.org/'
 
   app 'koto-Qt.app'
