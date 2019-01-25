b4e89ed014e33edf7af9ae0f67d1a27d901317c5
Cj-bc <cj-bc@y-modify.org>
2019-01-25 11:00:22 +0900

    Fix: koto-man> URL fixed; remove 'version' because url contains version

--- koto-man.rb
+++ koto-man.rb
@@ -1,8 +1,7 @@
 class KotoMan < Formula
   desc "man pages of Koto"
   homepage "https://github.com/KotoDevelopers/Koto"
-  url "https://github.com/KotoDevelopers/Koto/archive/v#{version}.tar.gz"
-  version "2.0.2"
+  url "https://github.com/KotoDevelopers/Koto/archive/v2.0.2.tar.gz"
   head "https://github.com/KotoDevelopers/Koto.git"
 
   conflicts_with "Cj-bc/wallets/koto", :because => "Both install man pages for koto"
