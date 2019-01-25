cf641f7530193037807e40845f6a21a1866e86ce
Cj-bc <cj-bc@y-modify.org>
2019-01-25 11:06:12 +0900

    Add: koto-man> sha256 hash for 2.0.2

--- koto-man.rb
+++ koto-man.rb
@@ -2,6 +2,7 @@ class KotoMan < Formula
   desc "man pages of Koto"
   homepage "https://github.com/KotoDevelopers/Koto"
   url "https://github.com/KotoDevelopers/Koto/archive/v2.0.2.tar.gz"
+  sha256 "41e3507f7c60c345950c097bea12cdf31e54d0eefc7c28db1aafe7c8ec7bab21"
   head "https://github.com/KotoDevelopers/Koto.git"
 
   conflicts_with "Cj-bc/wallets/koto", :because => "Both install man pages for koto"
