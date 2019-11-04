8dd2ff0510e43214e8e8d90c773ecfb1b35ba24b
Cj-bc <cj.bc-sd@outlook.jp>
2019-11-04 22:57:21 +0900

    Fix: SanDeGo> rename by using proper token generated by cask tool
    Generated by:
    ```
    $ "$(brew --repository)/Library/Taps/caskroom/homebrew-cask/developer/bin/generate_cask_token" 'SanDeGo-Qt'
    ```

--- Casks/sandego.rb
+++ Casks/sandego.rb
@@ -1,11 +1,11 @@
-cask 'SanDeGo-Qt' do
+cask 'sandego' do
   version '4.1.0'
   sha256 '2ccdbabc8d5f52b5a0835e5902fc27318a4477acfd378eded4d4b9b5569ce883'
 
   # github.com/SanDeGoProject/SanDeGo was verified as official when first introduced to the cask
   url "https://github.com/SanDeGoProject/SanDeGo/releases/download/4.1.0/sandego-4.1.0.0-mac.dmg"
   appcast 'https://github.com/SanDeGoProject/SanDeGo/releases.atom'
-  name 'SanDeGo-Qt'
+  name 'sandego'
   homepage 'https://sandego.net'
 
   app 'SanDeGo-Qt.app'
