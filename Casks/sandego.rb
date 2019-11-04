6f10b8315b1ee5510ccd9de7b9b2972be9935f64
Cj-bc <cj.bc-sd@outlook.jp>
2019-11-04 20:18:59 +0900

    Add: SanDeGo Qt wallet

---
+++ Casks/sandego.rb
@@ -0,0 +1,12 @@
+cask 'SanDeGo' do
+  version '4.1.0'
+  sha256 '2ccdbabc8d5f52b5a0835e5902fc27318a4477acfd378eded4d4b9b5569ce883'
+
+  # github.com/SanDeGoProject/SanDeGo was verified as official when first introduced to the cask
+  url "https://github.com/SanDeGoProject/SanDeGo/releases/download/4.1.0/sandego-4.1.0.0-mac.dmg"
+  appcast 'https://github.com/SanDeGoProject/SanDeGo/releases.atom'
+  name 'SanDeGo'
+  homepage 'https://sandego.net'
+
+  app 'SanDeGo-Qt.app'
+end
