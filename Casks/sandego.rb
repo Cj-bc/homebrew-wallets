4e0e429205aaca59f7ff4b38976112384d5679fe
Cj-bc <cj.bc-sd@outlook.jp>
2019-11-04 23:00:09 +0900

    Fix: SanDeGo> fix `brew cask style`

--- Casks/sandego.rb
+++ Casks/sandego.rb
@@ -3,10 +3,10 @@ cask 'sandego' do
   sha256 '2ccdbabc8d5f52b5a0835e5902fc27318a4477acfd378eded4d4b9b5569ce883'
 
   # github.com/SanDeGoProject/SanDeGo was verified as official when first introduced to the cask
-  url "https://github.com/SanDeGoProject/SanDeGo/releases/download/4.1.0/sandego-4.1.0.0-mac.dmg"
+  url 'https://github.com/SanDeGoProject/SanDeGo/releases/download/4.1.0/sandego-4.1.0.0-mac.dmg'
   appcast 'https://github.com/SanDeGoProject/SanDeGo/releases.atom'
   name 'sandego'
-  homepage 'https://sandego.net'
+  homepage 'https://sandego.net/'
 
   app 'SanDeGo-Qt.app'
 end
